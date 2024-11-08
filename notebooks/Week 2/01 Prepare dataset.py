1b88805b481f0796f12e7ba275fa00c46ee96484
Xander Schevenhoven <82219086+XanderSchevenhoven@users.noreply.github.com>
2024-11-08 12:52:33 +0100

    added simple model training, logging & registering notebook

--- notebooks/Week 2/01 Prepare dataset.py
+++ notebooks/Week 2/01 Prepare dataset.py
@@ -1,11 +1,10 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Excercise week 1 - preprocessing using python package
+# MAGIC # Excercise week 2 - preprocessing using python package
 # MAGIC Steps:
 # MAGIC 1. Load / open file
 # MAGIC 2. Preprocess data
-# MAGIC 3. Train simple model
-# MAGIC 4. Evaluate model
+# MAGIC 3. Save to unity catalog
 
 # COMMAND ----------
 # MAGIC %md ## I. Setup notebook
